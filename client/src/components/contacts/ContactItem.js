81b3c39f1da361dd0fb194d6d8f4e3a606550182
berkayyildirim <berkay.yildirim@yahoo.com>
2021-01-30 10:34:47 +0300

    set and clear current contact

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -4,10 +4,11 @@ import ContactContext from "../../context/contact/contactContext";
 
 const ContactItem = ({contact}) => {
     const contactContext = useContext(ContactContext);
-    const {deleteContact} = contactContext;
+    const {deleteContact, setCurrent, clearCurrent} = contactContext;
 
     const onDelete = () => {
         deleteContact(id);
+        clearCurrent();
     }
 
     const {id, name, email, phone, type} = contact;
@@ -31,7 +32,7 @@ const ContactItem = ({contact}) => {
                 </li>)}
             </ul>
             <p>
-                <button className="btn btn-dark btn-sm">Edit</button>
+                <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)} >Edit</button>
                 <button className="btn btn-danger btn-sm" onClick= {onDelete} >Delete</button>
             </p>
         </div>
